715415403efbae6de6306a86b084f525d72c987b
kimhyeji <gpwl5589@gmail.com>
2023-07-07 09:42:42 +0900

    특정 게시물을 찾는 메서드를 구현해서 중복제거

--- src/com/sbs/java/ssg/App.java
+++ src/com/sbs/java/ssg/App.java
@@ -60,16 +60,7 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -90,16 +81,7 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -118,16 +100,7 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				int foundIndex = -1;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundIndex = i;
-						break;
-					}
-				}
+				int foundIndex = getArticleIndexById(id);
 
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -144,6 +117,27 @@ public class App {
 
 		sc.close();
 		System.out.println("== 프로그램 끝 ==");
+	}
+
+	private int getArticleIndexById(int id) {
+		int i = 0;
+		for ( Article article : articles ) {
+			if (article.id == id) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+
+	private Article getArticleById(int id) {
+		int index = getArticleIndexById(id);
+		
+		if ( index != -1 ) {
+			return articles.get(index);
+		}
+		
+		return null;
 	}
 
 	private void makeTastData() {
