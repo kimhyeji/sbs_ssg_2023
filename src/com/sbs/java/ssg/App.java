873fa8caf1f766094775db02b93d8539121a440d
kimhyeji <gpwl5589@gmail.com>
2023-07-10 10:15:40 +0900

    프로그램 시작 시 테스트 회원 3명 생성

--- src/com/sbs/java/ssg/App.java
+++ src/com/sbs/java/ssg/App.java
@@ -15,7 +15,8 @@ public class App {
 		MemberController memberController = new MemberController(sc);
 		ArticleController articleController = new ArticleController(sc);
 		
-		articleController.makeTastData();
+		articleController.makeTestData();
+		memberController.makeTestData();
 
 		while (true) {
 			System.out.printf("명령어) ");
