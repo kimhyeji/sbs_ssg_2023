6646bf21a5343b1cdcc93fca87358d0005a21d30
kimhyeji <gpwl5589@gmail.com>
2023-07-11 09:45:06 +0900

    게시물 리스트에서 작성자 이름이 나오도록 수정

--- src/com/sbs/java/ssg/dto/Article.java
+++ src/com/sbs/java/ssg/dto/Article.java
@@ -4,7 +4,7 @@ public class Article extends Dto {
 	public String title;
 	public String body;
 	public int hit;
-	public int memberId; // 7
+	public int memberId;
 
 	public Article(int id, String regDate, int memberId, String title, String body) {
 		this(id, regDate, memberId, title, body, 0);
